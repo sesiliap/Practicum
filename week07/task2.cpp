791248d9022e8bfe1729080ece1cead829e2b0eb
sesiliap <115207883+sesiliap@users.noreply.github.com>
2022-11-18 08:56:18 +0200

    Create task2.cpp

---
+++ week07/task2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+const int MAX_SIZE = 100;
+
+int main() {
+    int N;
+    cin >> N;
+    
+    int arr[MAX_SIZE];
+    for(int i = 0; i < N; i++) {
+        
+    }
+    for(int i = 0; i < N; i++) {
+        if(i % 2 == 0) {
+            cout << i << " " << endl;
+        }
+    }
+    
+    return 0;
+}
