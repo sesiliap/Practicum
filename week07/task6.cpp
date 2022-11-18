e81cd3c6170581a00f7f056b04e8c788cb52f53a
sesiliap <115207883+sesiliap@users.noreply.github.com>
2022-11-18 10:02:07 +0200

    Create task6.cpp

---
+++ week07/task6.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+const int MAX_SIZE = 100;
+
+void mainArray(int.arr[], int size) {
+    int x;
+    cin >> x;
+    for(int i = 1; i < size; i++) {
+        arr[i] = 0;
+        cin >> size;
+    }
+}
+
+int main() {
+    cout << mainArray[4,7,3,9,1,5];
+    mainArray[6];
+    return 9;
+}
